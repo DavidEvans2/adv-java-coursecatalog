889f5ac522f700960b2dc824d7c2434d84b571b3
David Evans <devans31@my.wctc.edu>
2020-04-10 18:40:55 -0500

    Initial commit. Unfinished

--- src/lab1/IntroToProgrammingCourse.java
+++ src/lab1/IntroToProgrammingCourse.java
@@ -27,7 +27,7 @@ public class IntroToProgrammingCourse {
     public final void setCourseName(String courseName) {
         if (courseName == null || courseName.length() == 0) {
             JOptionPane.showMessageDialog(null,
-                    "Error: courseName cannot be null of empty string");
+                    "Error: courseName cannot be an empty string");
             System.exit(0);
         }
         this.courseName = courseName;
@@ -40,7 +40,7 @@ public class IntroToProgrammingCourse {
     public final void setCourseNumber(String courseNumber) {
         if (courseNumber == null || courseNumber.length() == 0) {
             JOptionPane.showMessageDialog(null,
-                    "Error: courseNumber cannot be null of empty string");
+                    "Error: courseNumber cannot be an empty string");
             System.exit(0);
         }
         this.courseNumber = courseNumber;
@@ -53,7 +53,7 @@ public class IntroToProgrammingCourse {
     public void setCredits(double credits) {
         if (credits < 0.5 || credits > 4.0) {
             JOptionPane.showMessageDialog(null,
-                    "Error: credits must be in the range 0.5 to 4.0");
+                    "Error: Number of credits  must be range from 0.5 to 4.0");
             System.exit(0);
         }
         this.credits = credits;
--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,4 +1,13 @@
 package lab1;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        //Declaring using Abstract class
+        CourseName newAdvancedJava = new AdvancedJavaCourse("AdvancedJava", "A113", "IntroToJava", 3);
+
+        //Declaring as Class Type
+        AdvancedJavaCourse extraAdvancedJava = new AdvancedJavaCourse("Advanced Java", "0007", "IntroToJava", 4);
+
+    }
 }
